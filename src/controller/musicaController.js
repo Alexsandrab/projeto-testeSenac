1c8df52de1dcab3800ad5f4a6e4d9a5bb0f574d2
Alexsandrab <sandrinhaleto@hotmail.com>
2021-11-26 21:11:25 -0300

    colocando o delete

--- src/controller/musicaController.js
+++ src/controller/musicaController.js
@@ -71,10 +71,64 @@ const createMusica = (req, res) => {
             res.status(500).send({ message: err }) // caso dê erro retorno status 500
         }
     }
-    
 
+    const updateMusicaStatus = (req, res) => {
+        try {
+            const musicaId = req.params.id // pego a informação do id no parametro da requisição
+            const favorita = req.body.favorita // pego a informação de watched no corpo da requisição. Ele terá valor true ou false, dependendo do que tiver sido passado
+    
+            const musicaToUpdate = musica.find(musica => musica.id == musicaId) // separo o filme que irei mudar o status
+            const musicaIndex = musica.indexOf(musicaToUpdate) // identifico o índice do filme no meu array
+    
+            if (musicaIndex >= 0) { // verifico se o filme existe no array de filmes
+                musicaToUpdate.favorita = favorita //atualizo o objeto com o novo status informando se foi assistido ou não
+                musica.splice(musicaIndex, 1, musicaToUpdate) // removo o filme pelo índice substituindo pelo novo
+            } else {
+                res.status(404).send({ message: "Musica não encontrada para informar se é favorita ou não" })
+            }
+    
+            fs.writeFile("./src/models/musica.json", JSON.stringify(musica), 'utf8', function (err) { // gravo meu json de filmes atualizado
+                if (err) {
+                    res.status(500).send({ message: err })
+                } else {
+                    console.log("Arquivo atualizado com sucesso!")
+                    const musicaUpdated = musica.find((musica) => musica.id == musicaId) // separo o filme que modifiquei no array
+                    res.status(200).send(musicaUpdated) // envio o filme modificado como resposta
+                }
+            })
+        } catch (err) {
+            res.status(500).send({ message: err })
+        }
+    }
+    const deleteMusica = (req, res) => {
+        try {
+            const musicaId = req.params.id
+            const musicaFound = musica.find(musica => musica.id == musicaId) // encontro o filme pelo id
+            const musicaIndex = musica.indexOf(musicaFound) // identifico o índice do filme no meu array
+    
+            if (musicaIndex >= 0) { // verifico se o filme existe no array de filmes
+                musica.splice(musicaIndex, 1) // removo o filme pelo índice
+            } else {
+                res.status(404).send({ message: "Música não encontrado para ser deletado" })
+            }
+    
+            fs.writeFile("./src/models/musica.json", JSON.stringify(musica), 'utf8', function (err) { // gravo meu array de filmes sem o filme que deletei
+                if (err) {
+                    res.status(500).send({ message: err })
+                } else {
+                    console.log("Titulo deletado com sucesso do arquivo!")
+                    res.sendStatus(204)
+                }
+            })
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({ message: "Erro ao deletar o filme" })
+        }
+    }
 
 module.exports = {
+    updateMusicaStatus,
+    deleteMusica,
     getTitulo,
     updateMusica,
     getMusica,
--- src/routes/musicaRoutes.js
+++ src/routes/musicaRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require ('../controller/musicaController');
 
+
+router.patch("/:id/favorita", controller.updateMusicaStatus)
+router.delete("/:id", controller.deleteMusica)
 router.get("/", controller.getAllMusica);
 router.post("/", controller.createMusica);
-//router.get("/:titulo", controller.getTitulo);
-//router.get("/:id", controller.getMusica);
+router.get("/:titulo", controller.getTitulo);
+router.get("/:id", controller.getMusica);
 router.put("/:id", controller.updateMusica);
 
 module.exports = router;
